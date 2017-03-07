d6ffb29142286e3e453b0eb860e672bdde600e40
rhaul-kumi <kayrahul@gmail.com>
2017-03-08 01:38:12 +0530

    vidCapt.py : under development

--- vidCapt.py
+++ vidCapt.py
@@ -12,9 +12,13 @@ while(cap.isOpened()):
     ret, frame = cap.read()
     if ret==True:
         file.write(frame)
-
-        cv2.imshow('frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('`1'):
+        frameGrayScl=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        frameGaussBlurr=cv2.GaussianBlur(frameGrayScl,(63,63),0)
+        ret,frameThreshd=cv2.threshold(frameGaussBlurr,127,255,cv2.THRESH_BINARY+cv2.ADAPTIVE_THRESH_GAUSSIAN_C)
+        # img,contours,heir=cv2.findContours(frameThreshd,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+        # print heir ##debugging
+        cv2.imshow('frame',frameThreshd)
+        if cv2.waitKey(1) & 0xFF == ord('`'):
             break
     else:
         break
